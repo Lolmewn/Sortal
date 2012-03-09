39157108a80ccbfd740d0e90231b29d09b4443bd
Lolmewn <Lolmewn@SybrensPC>
2012-03-09 16:36:15 +0100

    Fixed some small issues

--- src/main/java/nl/lolmen/sortal/Warp.java
+++ src/main/java/nl/lolmen/sortal/Warp.java
@@ -20,7 +20,7 @@ public class Warp{
 	private Main plugin;
 	private int cost;
 	private boolean hasCost = false;
-	
+
 	public Warp(Main main) {
 		plugin = main;
 	}
@@ -45,114 +45,81 @@ public class Warp{
 		this.cost = cost;
 		this.hasCost = true;
 	}
-	
+
 	/**
 	 * Saves the warp
 	 */
 	public void saveWarp() {
-		//if(!plugin.useSQL && !plugin.useMySQL){
-			try {
-				if(!this.warps.exists()){
-					this.warps.createNewFile();
-				}
-				Properties prop = new Properties();
-				FileInputStream in = new FileInputStream(this.warps);
-				prop.load(in);
-				prop.put(warp(), toString());
-				FileOutputStream out = new FileOutputStream(this.warps);
-				prop.store(out, "[WarpName]=[World],[X],[Y],[Z]");
-				this.plugin.warp.put(warp(), new Warp(this.plugin, this.warp, this.world, this.x,this.y,this.z));
-				in.close();
-				out.flush();
-				out.close();
-			} catch (IOException e) {
-				e.printStackTrace();
+		try {
+			if(!this.warps.exists()){
+				this.warps.createNewFile();
 			}
-		//}
-		/*if(plugin.useSQL){
-			plugin.sql.checkConnection();
-			plugin.sql.query("INSERT INTO Sortal (name, world, x,y,z,warp,cost) VALUES ('" + warp + "', '" + world + "', " + x + ", " + y + ", " + z + ", 1, 0;");
-			plugin.warp.put(warp(), new Warp(plugin, warp, world, x,y,z));
+			Properties prop = new Properties();
+			FileInputStream in = new FileInputStream(this.warps);
+			prop.load(in);
+			prop.put(warp(), toString());
+			FileOutputStream out = new FileOutputStream(this.warps);
+			prop.store(out, "[WarpName]=[World],[X],[Y],[Z]");
+			this.plugin.warp.put(warp(), new Warp(this.plugin, this.warp, this.world, this.x,this.y,this.z));
+			in.close();
+			out.flush();
+			out.close();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		if(plugin.useMySQL){
-			plugin.mysql.checkConnection();
-			try {
-				plugin.mysql.query("INSERT INTO Sortal (name, world, x,y,z,warp,cost) VALUES ('" + warp + "', '" + world + "', " + x + ", " + y + ", " + z + ", 1, 0;");
-				plugin.warp.put(warp(), new Warp(plugin, warp, world, x,y,z));
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}*/
 	}
 	public boolean delWarp(){
-		//if(!plugin.useSQL && !plugin.useMySQL){
-			try{
-				if(!this.warps.exists()){
-					this.warps.createNewFile();
-					return false;
-				}
-				Properties prop = new Properties();
-				FileInputStream in = new FileInputStream(this.warps);
-				prop.load(in);
-				if(prop.containsKey(this.warp)){
-					prop.remove(this.warp);
-					FileOutputStream out = new FileOutputStream(this.warps);
-					prop.store(out, "[WarpName]=[World],[X],[Y],[Z]");
-					this.plugin.warp.remove(this.warp);
-					out.flush();
-					out.close();
-					in.close();
-					return true;
-				}
-				in.close();
-				return false;
-			}catch(IOException e){
-				e.printStackTrace();
+		try{
+			if(!this.warps.exists()){
+				this.warps.createNewFile();
 				return false;
 			}
+			Properties prop = new Properties();
+			FileInputStream in = new FileInputStream(this.warps);
+			prop.load(in);
+			if(prop.containsKey(this.warp)){
+				prop.remove(this.warp);
+				FileOutputStream out = new FileOutputStream(this.warps);
+				prop.store(out, "[WarpName]=[World],[X],[Y],[Z]");
+				this.plugin.warp.remove(this.warp);
+				out.flush();
+				out.close();
+				in.close();
+				return true;
+			}
+			in.close();
+			return false;
+		}catch(IOException e){
+			e.printStackTrace();
+			return false;
+		}
 	}
 	public boolean setCost(int cost){
-		//if(!plugin.useSQL && !plugin.useMySQL){//
-			try{
-				if(!this.warps.exists()){
-					this.warps.createNewFile();
-					return false;
-				}
-				Properties prop = new Properties();
-				FileInputStream in = new FileInputStream(warps);
-				prop.load(in);
-				if(prop.containsKey(this.warp)){
-					prop.setProperty(this.warp, prop.getProperty(this.warp) + "," + cost);
-					FileOutputStream out = new FileOutputStream(warps);
-					prop.store(out, "[WarpName]=[World],[X],[Y],[Z]");
-					this.plugin.warp.remove(this.warp);
-					this.plugin.warp.put(this.warp, new Warp(this.plugin, this.warp, new Location(this.getWorld(), this.getX(), this.getY(), this.getZ()), this.cost));
-					out.flush();
-					out.close();
-					in.close();
-					return true;
-				}
+		try{
+			if(!this.warps.exists()){
+				this.warps.createNewFile();
+				return false;
+			}
+			Properties prop = new Properties();
+			FileInputStream in = new FileInputStream(warps);
+			prop.load(in);
+			if(prop.containsKey(this.warp)){
+				prop.setProperty(this.warp, this.toString());
+				FileOutputStream out = new FileOutputStream(warps);
+				prop.store(out, "[WarpName]=[World],[X],[Y],[Z]");
+				this.plugin.warp.remove(this.warp);
+				this.plugin.warp.put(this.warp, new Warp(this.plugin, this.warp, new Location(this.getWorld(), this.getX(), this.getY(), this.getZ()), this.cost));
+				out.flush();
+				out.close();
 				in.close();
-				return false;
-			}catch(IOException e){
-				e.printStackTrace();
-				return false;
+				return true;
 			}
-		//}
-		/*if(plugin.useSQL){
-			plugin.sql.checkConnection();
-			plugin.sql.query("UPDATE Sortal SET cost = " + cost+ " WHERE warp = '"+warp+ "';");
-			return true;
-		}else{
-			plugin.mysql.checkConnection();
-			try {
-				plugin.sql.query("UPDATE Sortal SET cost = " + cost+ " WHERE warp = '"+warp+ "';");
-			}  catch (Exception e) {
-				e.printStackTrace();
-				return false;
-			}
-			return true;
-		}*/
+			in.close();
+			return false;
+		}catch(IOException e){
+			e.printStackTrace();
+			return false;
+		}
 	}
 	public int getCost(){
 		if(this.cost == 0){
